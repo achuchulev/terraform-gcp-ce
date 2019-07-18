ffc90f2e155c60f1d4355a73e565aaebb46bd862
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-07-18 16:35:07 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 resource "null_resource" "HelloGCP" {
+  
+  triggers {
+        build_number = "${timestamp()}"
+  }
+  
   provisioner "local-exec" {
     command = "echo Hello GCP! Google provider is properly configured and authentication is working fine."
   }
+
 }
